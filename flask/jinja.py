a111565ab18fb20a5ee84a8e93750ee8413a2406
HARSHIT KAMRIYA <harshit_2024bite037@nitsri.ac.in>
2025-08-01 19:37:26 +0530

    working with flask

---
+++ flask/jinja.py
@@ -0,0 +1,102 @@
+# Building Url Dynamically
+# variable Rule
+# Jinja 2 Template Engine
+
+#### Jinja2 Template Engine
+'''
+    {{ }} expressions to print output in html
+    {%...%} conditions,for loops
+    {#...#} this is for comments
+'''
+
+
+from flask import Flask,render_template,request,redirect,url_for
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def welcome():
+    return "<html><H1> Welcome to the flask course</H1></html>"
+
+
+@app.route("/index",methods = ['GET'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+# @app.route('/submit',methods = ['GET','POST'])
+# def submit():
+#     if request.method == 'POST':
+#         name = request.form['name']
+#         return f'Hello {name}!'
+#     return render_template('form.html')
+
+# Variable Rule
+@app.route('/success/<int:score>')
+def success(score):
+    res = ""
+    if score>=50:
+        res = "PASSED"
+    else:
+        res = "FailED"
+
+    return render_template("result.html",results = res)
+
+
+
+@app.route('/successres/<int:score>')
+def successres(score):
+    res = ""
+    if score>=50:
+        res = "PASSED"
+    else:
+        res = "FailED"
+
+    exp = {'score': score,"res":res}
+
+    return render_template("result1.html",results = exp)
+
+
+# if condition
+@app.route('/sucessif/<int:score>')
+def sucessif(score):
+    return render_template("result.html",results = score)
+
+@app.route('/fail/<int:score>')
+def fail(score):
+    return render_template('result.html',results = score)
+
+
+@app.route('/submit',methods = ['POST','GET'])
+def submit():
+    total_score= 0
+    if request.method=='POST':
+        science= float(request.form['science'])
+        maths= float(request.form['maths'])
+        c= float(request.form['c'])
+        data_science = float(request.form['datascience'])
+
+
+
+        total_score= (science+maths+c+data_science)/4
+    else:
+        return render_template('getresult.html')
+    return redirect(url_for('successres',score = total_score))
+
+
+    
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
